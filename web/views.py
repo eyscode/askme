cf07cc77e9da49dda312c61eb5685c3718f6ab98
Eysenck Freddy Gómez Orihuela <eyscode@gmail.com>
2014-03-31 01:26:24 -0500

    # areglados estilos de formulario para la creación de preguntas

--- web/views.py
+++ web/views.py
@@ -1,19 +1,8 @@
 from django.core.urlresolvers import reverse_lazy
-from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 from web.mixins import SearchMixin
 from web.models import Pregunta
-
-
-def home(request):
-    q = request.GET.get('q')
-    if q:
-        ps = Pregunta.objects.filter(title__icontains=q)
-    else:
-        ps = Pregunta.objects.all()
-    context = {'preguntas': ps, 'q': q}
-    return render(request, 'lista_preguntas.html', context)
 
 
 class ListAsk(SearchMixin, ListView):
@@ -29,6 +18,6 @@ class ListAsk(SearchMixin, ListView):
 
 class FormAsk(CreateView):
     model = Pregunta
-    template_name = 'crea_pregunta2.html'
+    template_name = 'crea_pregunta.html'
     success_url = reverse_lazy('index')
     fields = ('title', 'content')
